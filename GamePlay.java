a475c7f6c7de89604a97567eacf67de95cbef2d3
rahul01-git <rahulneupane0090@gmail.com>
2023-04-06 22:30:01 +0545

    ball and pedal display

--- GamePlay.java
+++ GamePlay.java
@@ -1,3 +1,74 @@
-public class GamePlay {
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+public class GamePlay extends JPanel implements KeyListener,  ActionListener{
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+    
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+    
+
+    public GamePlay(){
+        addKeyListener(this);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay,this);
+        timer.start();
+    }
+
+    public void print(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(1, 1, 692, 592);
+
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 562);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        g.setColor(Color.blue);
+        g.fillRect(playerX, 550, 100, 8);
+
+        g.setColor(Color.green);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
     
 }
