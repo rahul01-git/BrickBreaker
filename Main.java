576b9c05f5409978ac7f3384f25cec2cf060b830
rahul01-git <rahulneupane0090@gmail.com>
2023-04-06 22:10:27 +0545

    main class development

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import javax.swing.JFrame;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        JFrame obj = new JFrame("Brick Breaker");
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
